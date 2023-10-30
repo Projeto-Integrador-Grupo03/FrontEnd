87710ec7a0b34676ddc6e26d1055ccb5092585e2
Fertilesoil <gasnbloop@gmail.com>
2023-10-30 10:04:36 -0300

    Navbar

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+﻿import '../navbar/Navbar.css'
+
+function Navbar() {
+   return (
+      <header>
+      <nav id="nav-flex">
+         <a className="link-nome" href="#">Iniciativa Atena</a>
+         <div id='div-flex'>
+            <ul id="ul-links">
+               <li><a className="link-format" href="#">Produtos</a></li>
+               <li><a className="link-format" href="#">Noticias</a></li>
+               <li><a className="link-format" href="#">Quem Somos</a></li>
+               <li><a className="link-format" href="#">Meu Perfil</a></li>
+               <li><a className="link-format" href="#">Carrinho</a></li>
+               <li><a className='link-format' href="#">Login</a></li>
+            </ul>
+         </div>
+      </nav>
+   </header>
+   )
+}
+
+export default Navbar
