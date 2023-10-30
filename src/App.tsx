9565850ee2d11dec9ef05702b0d929955f9dc4b7
Karina Akina <karinamzk2@gmail.com>
2023-10-30 14:10:38 -0300

    Construindo Home e Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <Login />
       <Footer />
     </>
   )
