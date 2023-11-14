c39f2e9b4595553b0f0a91f137d8cf5afe5df36a
samanthablazizza <samantha.blazizza@gmail.com>
2023-11-14 18:29:37 -0300

    Implementação do Carrossel

---
+++ src/components/carrossel/Carrossel.tsx
@@ -0,0 +1,56 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./Carrossel.css";
+
+import { Navigation, Pagination } from 'swiper/modules';
+
+function Carrossel() {
+
+    var items = [
+        { img: "https://s2.glbimg.com/JYZ14pT3LO5WFsHtpl3hm-p0k5M=/e.glbimg.com/og/ed/f/original/2015/10/26/mulheres-que-empreendem-videos.jpg" },
+        { img: "https://images.squarespace-cdn.com/content/v1/63937fe85df41f34b1fb94e9/106f7635-1846-45e9-885c-3d48a8e67208/CyAssist+-+banner+blog.jpg" },
+        { img: "https://blog.gentrop.com/hubfs/Post_MulheresnaTecnologia_Banner.png" },
+        { img: "https://img.freepik.com/vetores-premium/modelo-de-banner-de-campanha-de-feminismo_3870-450.jpg?w=2000" },
+        { img: "https://img.freepik.com/vetores-premium/minusculo-dermatologista-examinando-a-pagina-de-destino-de-pele-de-rosto-seco-em-estilo-plano_74855-19076.jpg?size=626&ext=jpg" }
+    ]
+
+    return (
+        <>
+            <Swiper 
+            style={{
+                "--swiper-pagination-color": "rgba(203, 95, 93, 0.39)",
+            }as CSSProperties}
+        
+            
+                slidesPerView={1}
+                spaceBetween={30}
+                loop={true}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+            >
+
+
+                {
+                    items.map((item) => (
+                        <SwiperSlide>
+                            <img src={item.img} alt="Imagem" />
+                        </SwiperSlide>
+                    ))
+                }
+
+            </Swiper>
+
+
+        </>
+    )
+}
+
+export default Carrossel
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,32 +1,17 @@
 import '../home/Home.css'
 import ListaProdutos from '../../components/produtos/listaProdutos/ListaProdutos'
-import ModalProdutos from '../../components/produtos/modalProdutos/ModalProdutos'
+//import ModalProdutos from '../../components/produtos/modalProdutos/ModalProdutos'
+import Carrossel from '../../components/carrossel/Carrossel'
 
 function Home() {
     return (
         <section className='flex flex-col'>
             <div id='caixahome'>
-                <div id='conteudohome'>
-                    <h1>
-                        O Projeto Iniciativa Atena Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text:
-                    </h1>
-                    <ul>
-                        <li>Lorem ipsum dolor </li>
-                        <li>Lorem ipsum dolor </li>
-                        <li>Lorem ipsum dolor </li>
-                    </ul>
-                    <div className="flex justify-around gap-4">
-                        <div className="flex justify-around gap-4">
-                            <ModalProdutos />
-                        </div>
-                    </div>
-                </div>
-                <div id='atena'>
-                    <img src="./src/assets/img/atena.png" alt="" />
-                </div>
+                <Carrossel />
             </div>
             <ListaProdutos />
         </section>
+
     )
 }
 
