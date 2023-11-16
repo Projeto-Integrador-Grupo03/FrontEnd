0fc73ad529736e5dd853bad910f0f2ca6cd328b5
Fertilesoil <gasnbloop@gmail.com>
2023-11-16 10:05:18 -0300

    Ajuste Navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,25 @@
 ﻿import { AuthContext } from '../../contexts/AuthContext';
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
+import { SignOut } from '@phosphor-icons/react';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
+
 import '../navbar/Navbar.css'
 
 
 function Navbar() {
-   const { usuario } = useContext(AuthContext);
+
+   const navigate = useNavigate();
+
+   const { usuario, handleLogout } = useContext(AuthContext)
+
+   function logout() {
+      handleLogout()
+      alert('Obrigado por usar nosso site!')
+      navigate('/login')
+   }
 
    return (
       <header>
@@ -27,13 +41,13 @@ function Navbar() {
 
             <div id='div-flex'>
                <ul id="ul-links">
-                  <Link to="/produtos"><li className='link-format'>Produtos</li></Link>
-                  <Link to="/categorias"><li className='link-format'>Categorias</li></Link>
-                  <Link to="/"><li className='link-format'>Noticias</li></Link>
-                  <Link to="/contato"><li className='link-format'>Quem Somos</li></Link>
-                  <Link to="/"><li className='link-format'>Meu Perfil</li></Link>
-                  <Link to="/"><li className='link-format'>Carrinho</li></Link>
-                  <Link to="/login"><li className='link-format'>Login</li></Link>
+                  <Link to="/produtos"><li className='link-format text-[white]'>Produtos</li></Link>
+                  <Link to="/categorias"><li className='link-format text-[white]'>Categorias</li></Link>
+                  <Link to="/"><li className='link-format text-[white]'>Canais de Apoio</li></Link>
+                  <Link to="/contato"><li className='link-format text-[white]'>Quem Somos</li></Link>
+                  <Link to="/"><li className='link-format text-[white]'>Carrinho</li></Link>
+                  <Link to="/login"><li className='link-format text-[white]'>Login</li></Link>
+                  <Link onClick={logout} to="/login"><i className='link-format cursor-pointer'><SignOut size={28} color="#fff5f5" weight="bold" /></i></Link>
                </ul>
             </div>
          </nav>
